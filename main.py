d575a85fcb64406efdd108e88eee84b14cb2d599
Krish Tanwar <krishtanwar458@gmail.com>
2025-03-09 01:17:31 -0800

    added description on new columns

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st #python3 -m streamlit run main.py
 
 st.set_page_config(layout='wide')
 
@@ -12,3 +12,8 @@ with col2:
     Hi, my name is Krish. 
     """
     st.info(content)
+
+content2 = 'Below you can find some of the apps I have builf in Python ' \
+            'Feel free to contact me!'
+
+st.text(content2)
