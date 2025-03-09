d394ab34f26bb26ecb85533d6f107e88ff4f611f
Krish Tanwar <krishtanwar458@gmail.com>
2025-03-09 01:09:28 -0800

    created columns, image, and description

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print('Hello World!')
-print('hello')
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.JPG', width=900)
+with col2:
+    st.title('Krish Tanwar')
+    content = """
+    Hi, my name is Krish. 
+    """
+    st.info(content)
