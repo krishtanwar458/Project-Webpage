a04728a2f08a1d586c21daa830f0e171e1a0d810
Krish Tanwar <krishtanwar458@gmail.com>
2025-04-29 09:53:09 -0700

    Added images, and description for each project

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ content2 = 'Below you can find some of the apps I have builf in Python ' \
 
 st.text(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 with col3:
     for i, row in df[:10].iterrows():
-        st.title(row['title'])
+        st.markdown(f"<h3 style='text-align: center;'>{row['title']}</h3>", unsafe_allow_html=True)
+        st.markdown(f"<p style='text-align: center;'>{row['description']}</p>", unsafe_allow_html=True)
+        st.image('images/' + row['image'])
+        st.markdown(f"<p style='text-align: center;'><a href='{row['url']}'>Source Code</a></p>", unsafe_allow_html=True)
 
 with col4:
     for i, row in df[10:].iterrows():
-        st.title(row['title'])
-        
+        st.markdown(f"<h3 style='text-align: center;'>{row['title']}</h3>", unsafe_allow_html=True) #st.write(row['title'])
+        st.markdown(f"<p style='text-align: center;'>{row['description']}</p>", unsafe_allow_html=True) #st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.markdown(f"<p style='text-align: center;'><a href='{row['url']}'>Source Code</a></p>", unsafe_allow_html=True) #st.write(f'[Source Code]({row['url']})')
