8e75af9464b111907c12d536b90dd373a720840f
Krish Tanwar <krishtanwar458@gmail.com>
2025-04-29 09:25:51 -0700

    Rendered app titles into columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st #python3 -m streamlit run main.py
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.JPG', width=900)
+    st.image('images/photo.JPG', width=700)
 with col2:
     st.title('Krish Tanwar')
     content = """
@@ -17,3 +18,15 @@ content2 = 'Below you can find some of the apps I have builf in Python ' \
             'Feel free to contact me!'
 
 st.text(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv('data.csv', sep=';')
+with col3:
+    for i, row in df[:10].iterrows():
+        st.title(row['title'])
+
+with col4:
+    for i, row in df[10:].iterrows():
+        st.title(row['title'])
+        
