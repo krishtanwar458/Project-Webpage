2e2d074f1cfa275046aabdf543e44d7e9d6dea4b
Krish Tanwar <krishtanwar458@gmail.com>
2025-06-24 12:27:05 +0530

    built an email sender page

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from send_email1 import send_email
 
 st.header('Contact Me')
 
 with st.form(key='email_form'):
-    user_emaiil = st.text_input('Your Email')
-    message = st.text_area('Your Message')
+    user_email = st.text_input('Your Email')
+    raw_message = st.text_area('Your Message')
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+""" 
     button = st.form_submit_button('Submit')
     if button:
-        print('Email Sent')
+        send_email(message)
+        st.info('Your email was sent successfully!')
